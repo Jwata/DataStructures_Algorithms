0e2e13f89c3e5582b4916157360f929ae91a2fc7
Jwata <watanabe0621@gmail.com>
2017-12-26 08:20:47 +0900

    binary tree in python

---
+++ binary_tree.py
@@ -0,0 +1,11 @@
+class BinaryTreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def add_to_left(self, value):
+        self.left = BinaryTreeNode(value)
+
+    def add_to_right(self, value):
+        self.right = BinaryTreeNode(value)
