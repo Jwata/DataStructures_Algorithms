dd03b7b4c524d3e5ba73ff4e70ea60cecc7b6d4a
Jwata <watanabe0621@gmail.com>
2018-07-30 15:16:50 +0900

    minor fix

--- interviewcake/cake_thief.py
+++ interviewcake/cake_thief.py
@@ -1,7 +1,12 @@
+import unittest
+
+
 def max_duffel_bag_value(cake_tuples, capacity):
     M = [0] * (capacity + 1)
 
     for w, v in cake_tuples:
+        if w == 0 and v > 0:
+            return float('inf')
         for c in range(capacity+1):
             if c < w:
                 continue
@@ -11,9 +16,48 @@ def max_duffel_bag_value(cake_tuples, capacity):
     return M[capacity]
 
 
-cake_tuples = [(7, 160), (3, 90), (2, 15)]
-capacity    = 20
+class Test(unittest.TestCase):
 
-# Returns 555 (6 of the middle type of cake and 1 of the last type of cake)
-max_value = max_duffel_bag_value(cake_tuples, capacity)
-print(max_value)
+    def test_one_cake(self):
+        actual = max_duffel_bag_value([(2, 1)], 9)
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_two_cakes(self):
+        actual = max_duffel_bag_value([(4, 4), (5, 5)], 9)
+        expected = 9
+        self.assertEqual(actual, expected)
+
+    def test_only_take_less_valuable_cake(self):
+        actual = max_duffel_bag_value([(4, 4), (5, 5)], 12)
+        expected = 12
+        self.assertEqual(actual, expected)
+
+    def test_lots_of_cakes(self):
+        actual = max_duffel_bag_value([(2, 3), (3, 6), (5, 1), (6, 1), (7, 1), (8, 1)], 7)
+        expected = 12
+        self.assertEqual(actual, expected)
+
+    def test_value_to_weight_ratio_is_not_optimal(self):
+        actual = max_duffel_bag_value([(51, 52), (50, 50)], 100)
+        expected = 100
+        self.assertEqual(actual, expected)
+
+    def test_zero_capacity(self):
+        actual = max_duffel_bag_value([(1, 2)], 0)
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_cake_with_zero_value_and_weight(self):
+        actual = max_duffel_bag_value([(0, 0), (2, 1)], 7)
+        expected = 3
+        self.assertEqual(actual, expected)
+
+    def test_cake_with_non_zero_value_and_zero_weight(self):
+        actual = max_duffel_bag_value([(0, 5)], 5)
+        expected = float('inf')
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
+
