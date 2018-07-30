635ffdb6f08437166cf1b8389aaf517c3c552c34
Jwata <watanabe0621@gmail.com>
2018-07-30 21:19:51 +0900

    tree traversal

--- interviewcake/balanced_binary_tree.py
+++ interviewcake/balanced_binary_tree.py
@@ -1,76 +1,121 @@
-class BinaryTreeNode:
+import unittest
 
-  def __init__(self, value):
-      self.value = value
-      self.left  = None
-      self.right = None
 
-  def insert_left(self, value):
-      self.left = BinaryTreeNode(value)
-      return self.left
-
-  def insert_right(self, value):
-      self.right = BinaryTreeNode(value)
-      return self.right
-
-def is_superbalanced(root):
-
-    nodes = []
-    if root.left:
-        nodes.append(root.left)
-    if root.right:
-        nodes.append(root.right)
-
-    depth = 1
-    min_depth = None
-
+# TODO: better to use DFS as it reaches to a leaf easier than BFS
+def is_balanced(root):
+    # BFS
+    nodes = [root]
     children = []
-    current = None
-
+    min_depth = None
+    depth = 0
     while len(nodes) > 0:
-        current = nodes.pop()
+        current = nodes.pop(0)
+
+        if not min_depth:
+            if not current.left and not current.right:
+                min_depth = depth
+        else:
+            if depth > min_depth + 1:
+                return False
+
 
         if current.left:
             children.append(current.left)
         if current.right:
             children.append(current.right)
 
-        if not current.left and not current.right:
-            if not min_depth:
-                min_depth = depth
-
+        # Go to next depth
         if len(nodes) == 0:
-            depth += 1
             nodes = children
             children = []
+            depth += 1
 
-    return depth - min_depth <= 1
+    return True
 
-# depth 0
-root = BinaryTreeNode(1)
 
-# depth 1
-root.insert_left(2)
-root.insert_right(3)
+class Test(unittest.TestCase):
+    class BinaryTreeNode:
+        def __init__(self, value):
+            self.value = value
+            self.left = None
+            self.right = None
 
-# depth 2
-root.left.insert_left(4)
-root.right.insert_left(5)
+        def insert_left(self, value):
+            self.left = Test.BinaryTreeNode(value)
+            return self.left
 
-# depth 3
-root.left.left.insert_left(6)
+        def insert_right(self, value):
+            self.right = Test.BinaryTreeNode(value)
+            return self.right
 
-print(is_superbalanced(root)) # should be False
+    def test_full_tree(self):
+        tree = Test.BinaryTreeNode(5)
+        left = tree.insert_left(8)
+        right = tree.insert_right(6)
+        left.insert_left(1)
+        left.insert_right(2)
+        right.insert_left(3)
+        right.insert_right(4)
+        result = is_balanced(tree)
+        self.assertTrue(result)
 
-# depth 0
-root = BinaryTreeNode(1)
+    def test_both_leaves_at_the_same_depth(self):
+        tree = Test.BinaryTreeNode(3)
+        left = tree.insert_left(4)
+        right = tree.insert_right(2)
+        left.insert_left(1)
+        right.insert_right(9)
+        result = is_balanced(tree)
+        self.assertTrue(result)
 
-# depth 1
-root.insert_left(2)
-root.insert_right(3)
+    def test_leaf_heights_differ_by_one(self):
+        tree = Test.BinaryTreeNode(6)
+        left = tree.insert_left(1)
+        right = tree.insert_right(0)
+        right.insert_right(7)
+        result = is_balanced(tree)
+        self.assertTrue(result)
 
-# depth 2
-root.left.insert_left(4)
-root.right.insert_left(5)
+    def test_leaf_heights_differ_by_two(self):
+        tree = Test.BinaryTreeNode(6)
+        left = tree.insert_left(1)
+        right = tree.insert_right(0)
+        right_right = right.insert_right(7)
+        right_right.insert_right(8)
+        result = is_balanced(tree)
+        self.assertFalse(result)
 
-print(is_superbalanced(root)) # should be True
+    def test_three_leaves_total(self):
+        tree = Test.BinaryTreeNode(1)
+        left = tree.insert_left(5)
+        right = tree.insert_right(9)
+        right.insert_left(8)
+        right.insert_right(5)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+    def test_both_subtrees_superbalanced(self):
+        tree = Test.BinaryTreeNode(1)
+        left = tree.insert_left(5)
+        right = tree.insert_right(9)
+        right_left = right.insert_left(8)
+        right.insert_right(5)
+        right_left.insert_left(7)
+        result = is_balanced(tree)
+        self.assertFalse(result)
+
+    def test_only_one_node(self):
+        tree = Test.BinaryTreeNode(1)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+    def test_linked_list_tree(self):
+        tree = Test.BinaryTreeNode(1)
+        right = tree.insert_right(2)
+        right_right = right.insert_right(3)
+        right_right.insert_right(4)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
