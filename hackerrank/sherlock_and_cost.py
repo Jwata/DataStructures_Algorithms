7c25c7bb20fd7dd27669a94b71a87effbedaf6da
Jwata <watanabe0621@gmail.com>
2018-08-02 16:15:19 +0900

    improve time efficiency

--- hackerrank/sherlock_and_cost.py
+++ hackerrank/sherlock_and_cost.py
@@ -8,27 +8,24 @@ import sys
 
 # Complete the cost function below.
 def cost(B):
-    i = 0
     j = 1
-
-    dp = {}
-    for v in range(1, B[0]+1):
-        dp[v] = 0
+    dp = ((1, 0), (B[0], 0))
 
     while j < len(B):
-        new_dp = {}
-        for vj in range(1, B[j]+1):
-            max_cost_vj = 0
-            for vi, cost in dp.items():
-                cost_vj_vi = cost + abs(vj - vi)
-                max_cost_vj = max([max_cost_vj, cost_vj_vi])
-            new_dp[vi] = max_cost_vj
-        # update params
-        dp = new_dp
-        i+=1
+        (min_ai, cost_at_min_ai), (max_ai, cost_at_max_ai) = dp
+        min_aj, max_aj = 1, B[j]
+
+        max_cost_at_min_aj = cost_at_max_ai + abs(max_ai - min_aj)
+        max_cost_at_max_aj = max([
+            cost_at_min_ai + abs(max_aj - min_ai),
+            cost_at_max_ai + abs(max_ai - max_aj)
+        ])
+
+        dp = ((min_aj, max_cost_at_min_aj), (max_aj, max_cost_at_max_aj))
         j+=1
 
-    return max(dp.values())
+    (_, cost1), (_, cost2) = dp
+    return max([cost1, cost2])
 
 if __name__ == '__main__':
     Bs = [
