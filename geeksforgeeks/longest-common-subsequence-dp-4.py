6f760683d4532049aa6f2a72f524846ad1f59ef5
Jwata <watanabe0621@gmail.com>
2019-02-27 22:15:02 +0900

    another longest common sequence solution

--- geeksforgeeks/longest-common-subsequence-dp-4.py
+++ geeksforgeeks/longest-common-subsequence-dp-4.py
@@ -22,7 +22,7 @@ def L_dp(x, y):
         for j in range(m + 1):
             if i == 0 or j == 0:
                 dp[i][j] = 0
-            elif x[i-1] == y[j-1]:
+            elif x[i - 1] == y[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1] + 1
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
@@ -32,3 +32,24 @@ def L_dp(x, y):
 
 print(L_dp("ABCDGH", "AEDFHE"))
 print(L_dp("AGGTAB", "GXTXAYB"))
+
+
+def L_dp2(str1, str2, i, j, dp):
+    if dp[i][j] is not None:
+        return dp[i][j]
+
+    if i == 0  or j == 0:
+        dp[i][j] = 0
+    elif str1[i-1] == str2[j-1]:
+        dp[i][j] = L_dp2(str1, str2, i - 1, j - 1, dp) + 1
+    else:
+        dp[i][j] = max(
+            L_dp2(str1, str2, i - 1, j, dp), L_dp2(str1, str2, i, j - 1, dp))
+
+    return dp[i][j]
+
+
+dp = [[None] * 7 for _ in range(7)]
+print(L_dp2("ABCDGH", "AEDFHE", 6, 6, dp))
+dp = [[None] * 8 for _ in range(7)]
+print(L_dp2("AGGTAB", "GXTXAYB", 6, 7, dp))
