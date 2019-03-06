eac699a4b9e2dcf4a6a83fc19fa39f5828148c9c
Jwata <watanabe0621@gmail.com>
2019-03-06 10:02:32 +0900

    insertion sort

---
+++ geeksforgeeks/insertion-sort.py
@@ -0,0 +1,13 @@
+# O(n^2)
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+            else:
+                break
+    return arr
+
+arr = [12, 11, 13, 5, 6]
+print(insertion_sort(arr)) 
