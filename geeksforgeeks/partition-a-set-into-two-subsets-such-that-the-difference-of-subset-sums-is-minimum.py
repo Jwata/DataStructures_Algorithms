35321644d7125ea465a26ad267c09f839a85e423
Jwata <watanabe0621@gmail.com>
2019-03-15 22:07:35 +0900

    dp solution

--- geeksforgeeks/partition-a-set-into-two-subsets-such-that-the-difference-of-subset-sums-is-minimum.py
+++ geeksforgeeks/partition-a-set-into-two-subsets-such-that-the-difference-of-subset-sums-is-minimum.py
@@ -1,5 +1,6 @@
 import heapq
 
+# wrong answer
 def partition(arr):
     sub1 = []
     sum1 = 0
@@ -29,6 +30,40 @@ def partition(arr):
 
     return abs(sum1 - sum2)
 
-arr = [1, 6, 11, 5]
+def find_min_diff(arr):
+    n = len(arr)
+    s = sum(arr)
 
-print(partition(arr)) # 1
+    dp = [[None for _ in range(s+1)] for _ in range(n+1)]
+
+    for i in range(n+1):
+        dp[i][0] = True
+
+    for j in range(1,s+1):
+        dp[0][j] = False
+
+    for i in range(1, n+1):
+        for j in range(1,s+1):
+            dp[i][j] = dp[i-1][j]
+
+            if j >= arr[i-1]:
+                dp[i][j] = dp[i][j] or dp[i-1][j-arr[i-1]]
+
+    # # for debuging
+    # print(arr)
+    # for i in range(n+1):
+    #     print(dp[i])
+
+    j = s//2
+    while j >= 0:
+        if dp[n][j]:
+            diff = (s - j) - j
+            break
+        j -= 1
+
+    return diff
+
+# arr = [1, 6, 11, 5]
+arr = [3, 1, 4, 2, 2, 1]
+
+print(find_min_diff(arr)) # 1
