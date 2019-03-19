b52eb8d27a79653009409e0848a50b0366dc4e86
Jwata <watanabe0621@gmail.com>
2019-03-20 06:11:40 +0900

    modify heap to implement priority queue

---
+++ geeksforgeeks/priority_queue.py
@@ -0,0 +1,63 @@
+class PriorityQueue:
+    def __init__(self):
+        self.arr = []
+
+    def swap(self, i, j):
+        self.arr[i], self.arr[j] = self.arr[j], self.arr[i]
+
+    def size(self):
+        return len(self.arr)
+
+    def empty(self):
+        return self.size() == 0
+
+    def head(self):
+        return self.arr[0] # raise error if heap is empty
+
+    def push(self, p, q):
+        self.arr.append((p, q))
+
+        i = self.size() - 1
+        while i > 0:
+            j = (i-1)//2
+            if self.arr[j][0] < self.arr[i][0]:
+                break
+
+            self.swap(i, j)
+            i = j
+
+    def pop(self):
+        if self.size() == 1:
+            return self.arr.pop()
+
+        root = self.arr[0] # raise error if heap is empty
+        self.arr[0] = self.arr.pop()
+
+        i = 0
+        while i < self.size():
+            l = i * 2 + 1
+            r = l + 1
+
+            if l < self.size() and self.arr[l][0] < self.arr[i][0]:
+                self.swap(i, l)
+                i = l
+            elif r < self.size() and self.arr[r][0] < self.arr[i][0]:
+                self.swap(i, r)
+                i = r
+            else:
+                break
+
+        return root
+
+
+h = PriorityQueue()
+h.push(4, "tokyo")
+h.push(8, "nagoya")
+h.push(11, "osaka")
+h.push(7, "sapporo")
+h.push(3, "fukuoka")
+h.push(9, "naha")
+print(h.arr)
+
+while not h.empty():
+    print(h.pop(), h.arr)
