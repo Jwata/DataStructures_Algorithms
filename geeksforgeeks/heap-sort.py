93999aeb0753c13e0c531855fee6a5bd4aac85bf
Jwata <watanabe0621@gmail.com>
2019-03-22 12:31:13 +0900

    heap sort

--- geeksforgeeks/heap-sort.py
+++ geeksforgeeks/heap-sort.py
@@ -15,9 +15,16 @@ def heapify(arr, n, i):
 def swap(arr, i, j):
     arr[i], arr[j] = arr[j], arr[i]
 
-arr = [4, 10, 3, 5, 15]
-n = len(arr)
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n-1, -1, -1):
+        heapify(arr, n, i)
 
-for i in range(n-1, -1, -1):
-    heapify(arr, n, i)
+    for i in range(n):
+        swap(arr, 0, n-1-i)
+        heapify(arr, n-1-i, 0)
+
+
+arr = [4, 10, 3, 5, 15]
+heap_sort(arr)
 print(arr)
