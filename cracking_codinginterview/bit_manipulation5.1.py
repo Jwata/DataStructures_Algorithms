514b6a5c2051cfb485744c7631f0e3e42249849b
Jwata <watanabe0621@gmail.com>
2019-03-30 01:16:11 +0900

    insert bit number

---
+++ cracking_codinginterview/bit_manipulation5.1.py
@@ -0,0 +1,26 @@
+import math
+
+NUM_BITS = 32
+
+def insert_bit(n, m, i, j):
+    mask = 0
+    num_n = int(math.log2(n)) + 1
+    for k in range(num_n):
+        if k in range(i, j+1):
+            continue
+        mask |= (1 << k)
+
+    print('mask: {:b}'.format(mask))
+
+    n &= mask
+    n |= (m << i)
+
+    return n
+
+n = 1 << 10 # 10000000000
+m = 19 # 10011
+i = 2
+j = 6
+
+n = insert_bit(n, m, i, j)
+print('out: {:b}'.format(n))
