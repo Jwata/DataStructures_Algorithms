5d0e1206d309a2ad65499f5a2b11851b62405a9e
Jwata <watanabe0621@gmail.com>
2019-04-02 09:44:28 +0900

    optimal solution

--- cracking_codinginterview/bit_manipulation5.3.py
+++ cracking_codinginterview/bit_manipulation5.3.py
@@ -1,5 +1,7 @@
 import math
 
+# Let b is the lengts of bits of a
+# Time O(b), Space O(b)
 def flip_longest(a):
     cnt = 0
     seqs = []
@@ -20,10 +22,32 @@ def flip_longest(a):
 
     return longest
 
-## Debug
-# a = 0 # 0
-# seqs = [1, 0, 1, 0, 2]
-# cnt = 2
-# longest = 4
+# Time: O(b), Space: O(1)
+def flip_longest_optimal(a):
+    seq = 0
+    prev_seq = 0
+    longest = 1
 
-print(flip_longest(1775)) # 8
+    while a > 0:
+        if a & 1:
+            seq += 1
+        else:
+            if a & 2:
+                prev_seq = seq
+            else:
+                prev_seq = 0
+            seq = 0
+
+        longest = max(seq+prev_seq+1, longest)
+        a >>= 1
+
+    return longest
+
+# Debug
+# a_2 = 10
+# seq = 0
+# prev_seq = 1
+# longest = 2
+
+print(flip_longest_optimal(1775)) # 11011101111 -> 8
+print(flip_longest_optimal(1700)) # 11010100100 -> 4
