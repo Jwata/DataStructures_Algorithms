fce5015ab3d4cdbf77f9e14cdbc2f6faeb0ae871
Jwata <watanabe0621@gmail.com>
2019-05-09 00:18:01 +0900

    modify to return subarray indexes

--- geeksforgeeks/find-the-maximum-subarray-xor-in-a-given-array2.py
+++ geeksforgeeks/find-the-maximum-subarray-xor-in-a-given-array2.py
@@ -12,14 +12,16 @@ class Trie:
 
     def __init__(self):
         self.root = Node()
+        self.index = None
 
-    def add(self, v):
+    def add(self, v, index):
         root = self.root
         for i in range(self.NUM_BITS-1, -1, -1):
             if v & (1 << i) > 0:
                 root = root.next(1)
             else:
                 root = root.next(0)
+        root.index = index
 
     def search_max_xor(self, v):
         max_xor = 0
@@ -36,12 +38,10 @@ class Trie:
             if root.children[b_op]: # go to opposite direction
                 root = root.next(b_op)
                 max_xor |= (1 << i)
-            elif root.children[b]: # go to same 
+            else: # go to same 
                 root = root.next(b)
-            else:
-                break
 
-        return max_xor
+        return max_xor, root.index
 
 def find_max_xor(arr):
     n = len(arr)
@@ -50,17 +50,21 @@ def find_max_xor(arr):
 
     cur_xor = 0
     max_xor = 0
+    from_index = 0
+    to_index = 0
     trie = Trie()
 
     for i in range(n):
         v = arr[i]
         cur_xor ^= v
-        trie.add(cur_xor)
-        xor = trie.search_max_xor(cur_xor)
+        trie.add(cur_xor, i)
+        xor, index = trie.search_max_xor(cur_xor)
         if xor > max_xor:
             max_xor = xor
+            from_index = index + 1
+            to_index = i
 
-    return max_xor
+    return max_xor, from_index, to_index
 
 print(find_max_xor([4, 6]))
 print(find_max_xor([1, 2, 3, 4]))
